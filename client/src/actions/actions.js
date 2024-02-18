4b3cf6ac8f1fc9043ac530a152100aac0cd03530
Lexie5212 <li.xiaolu1@northeastern.edu>
2024-02-18 15:24:10 -0800

    add client files

---
+++ client/src/actions/actions.js
@@ -0,0 +1,53 @@
+import * as api from '../api';
+import { FETCH_ALL, CREATE, UPDATE, DELETE, LIKE } from '../constants/actionTypes';
+
+//action creators
+export const getPosts = () => async(dispatch) => {//dispatch from redux-thunk
+    // const action = { type:'FETCH_ALL', payload:[]}
+
+    // dispatch(action);
+    try {
+        const { data } = await api.fetchPosts();//fetching data from api
+        dispatch({ type: FETCH_ALL, payload: data }) ;//sending data thr action payload,in the reducer,return action.payload
+
+     } catch (error) {
+        console.log(error);
+    }
+}
+export const createPost = (post) => async(dispatch) =>{
+    try{
+        const { data } = await api.createPost(post);
+        dispatch({ type:CREATE, payload: data })
+    }
+    catch(error){
+        console.log(error);
+    }
+}
+export const updatePost = (id, post) => async (dispatch) => {
+    try {
+      const { data } = await api.updatePost(id, post);
+  
+      dispatch({ type: UPDATE, payload: data });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  export const deletePost = (id) => async (dispatch) => {
+    try {
+      await api.deletePost(id);
+  
+      dispatch({ type: DELETE, payload: id });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  export const likePost = (id) => async (dispatch) => {
+    try {
+      const { data } = await api.likePost(id);
+  
+      dispatch({ type: LIKE, payload: data });
+    } catch (error) {
+      console.log(error);
+    }
+  };
